a45c5f77c285acfef1b56b904efccc5d7c4957d8
SVladkov <s.v.vladkov@gmail.com>
2014-07-23 18:45:56 +0300

    Modified pictures, limited cup movements, properly placed liquid bubbles

--- TimeWaste/src/com/timewaste/games/drinkfarm/DrinkFarmLogic.java
+++ TimeWaste/src/com/timewaste/games/drinkfarm/DrinkFarmLogic.java
@@ -1,8 +1,10 @@
 package com.timewaste.games.drinkfarm;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
+import java.util.LinkedList;
 
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
@@ -18,12 +20,17 @@ public class DrinkFarmLogic {
 	private Sprite game_cup;
 	private Sprite game_bubble;
 	
+	private int[] liquid_bubble_y = new int[10];
+	
+	private int cup_top_limit;
+	private int cup_bottom_limit;
 	
 	private int device_width;
     private int device_height;
 	
 	private DrinkFarm game_instance;
 	private Map<String, ITextureRegion> textures = new TreeMap<String, ITextureRegion>();
+	private LinkedList<Sprite> liquid_bubbles = new LinkedList<Sprite>();
 	
 	private int screen_width() {
 		return game_instance.cameraWidth();
@@ -33,10 +40,14 @@ public class DrinkFarmLogic {
 		return game_instance.cameraHeight();
 	}
 	
-	private Sprite set_cup_logic(){
+	private Sprite set_cup_logic(){ 
 		return new Sprite(0, 0, textures.get("cup"), game_instance.getVertexBufferObjectManager()) {
 			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
-				this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2, pSceneTouchEvent.getY() - this.getHeight() / 2);
+				float new_position_x = pSceneTouchEvent.getX() - this.getWidth() / 2;
+				float new_position_y = pSceneTouchEvent.getY() - this.getHeight() / 2;
+				if(new_position_y > cup_top_limit && new_position_y < cup_bottom_limit) {
+					this.setPosition(new_position_x, new_position_y);
+				}
 				return true;
 			}
 		};
@@ -46,23 +57,35 @@ public class DrinkFarmLogic {
 		TimerHandler mTimerHandler = new TimerHandler(0.01f, true, new ITimerCallback() {
 		    @Override
 		    public void onTimePassed(TimerHandler pTimerHandler) {
-		    	game_bubble.setPosition(game_bubble.getX(), game_bubble.getY() + 1);
+		    	Iterator<Sprite> iter = liquid_bubbles.iterator();
+		    	while(iter.hasNext()) {
+		    		Sprite bubble = iter.next();
+		    		bubble.setPosition(bubble.getX(), bubble.getY() + 1);
+		    	}
 		    }
 		});
 		a_scene.registerUpdateHandler(mTimerHandler);
 	}
 	
-	private void set_ground_images(Scene a_scene) {      
-		float current_y = 0;
-		float current_x = 0;
+	private void make_bubbles(Scene a_scene) {
+		for(int i=0; i<5; i++) {
+			game_bubble = new Sprite(liquid_bubble_y[i], device_height / 2, textures.get("bubble"), game_instance.getVertexBufferObjectManager());		
+			a_scene.registerTouchArea(game_bubble);
+			a_scene.attachChild(game_bubble);
+			
+			liquid_bubbles.add(game_bubble);
+		}
+	}
+	
+	private void set_ground_images(Scene a_scene) {		
+		cup_top_limit = device_height * 9 / 16;
+		cup_bottom_limit = device_height * 5 / 8;
 		
 		game_cup = set_cup_logic();
-		game_cup.setPosition(current_x, current_y);
+		game_cup.setPosition(250, cup_bottom_limit);
 		
-		game_bubble = new Sprite(0, 200, textures.get("bubble"), game_instance.getVertexBufferObjectManager());
-		
-		a_scene.registerTouchArea(game_bubble);
-		a_scene.attachChild(game_bubble);
+		set_liquid_bubble_Ys();
+		make_bubbles(a_scene);
 		
 		a_scene.registerTouchArea(game_cup);
 		falling_liquid_logic(a_scene);
@@ -88,5 +111,13 @@ public class DrinkFarmLogic {
 		Random random_number = new Random(System.currentTimeMillis());
 		int random_int = random_number.nextInt(range);
 		return random_int;
+	}
+	
+	private void set_liquid_bubble_Ys() {
+		liquid_bubble_y[0] = device_width * 1 / 6;
+		liquid_bubble_y[1] = device_width * 2 / 6;
+		liquid_bubble_y[2] = device_width * 23 / 48;
+		liquid_bubble_y[3] = device_width * 31 / 48;
+		liquid_bubble_y[4] = device_width * 19 / 24;
 	}
 }
